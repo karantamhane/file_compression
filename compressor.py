d2ca6da02499d6b2b822c15a7105506aa5fbe526
Karan Tamhane <karan.tamhane@gmail.com>
2016-04-22 14:33:36 -0400

    Code clean up

--- compressor.py
+++ compressor.py
@@ -1,6 +1,4 @@
 import sys, struct
-# Use struct.pack('i', int('101010100011', 2)) to pack bitstring into binary form.
-
 
 def get_char_freqs(data):
   # Parse data to get char frequencies
@@ -11,32 +9,6 @@ def get_char_freqs(data):
     else:
       char_freqs[char] = 1
   return char_freqs
-
-
-def build_tree(freqs):
-  # Build an array respresentation of the tree (BAD!)
-  # Start with chars with lowerst freqs, construct parent, add to list and sort again
-  i = len(freqs)-2
-  while i >= 0:
-    freqs.append([None, freqs[i][1] + freqs[i+1][1]])
-    freqs = sorted(freqs, key=lambda (k,v):(v,k))[::-1]
-    i -= 1
-  tree = {}
-  leaves = []
-  count, freqs_ind = 1, 0
-  # Construct list with count number of total nodes needed to represent len(freqs) number of tree nodes
-  while freqs_ind < len(freqs):
-    key, val = freqs[freqs_ind]
-    if count/2 in leaves:
-      leaves.append(count)
-    else:
-      if key:
-        leaves.append(count)
-        tree[count] = key
-      freqs_ind += 1
-    count += 1
-  return tree
-
 
 def get_smallest_element(q1, q2):
   if q2:
@@ -49,23 +21,11 @@ def get_smallest_element(q1, q2):
 
 def build_tree_like_a_sane_person(freqs):
   freqs = [orig + [None, None] for orig in freqs]
-  # print freqs
   q1, q2 = freqs, []
   while len(q1) + len(q2) > 1:
     first, second = get_smallest_element(q1,q2), get_smallest_element(q1,q2)
     q2.insert(0, [None, first[1] + second[1], first, second])
   return q2
-
-# Old function as a souvenir
-# def get_encoding(ind):
-#   enc = ''
-#   while ind > 1:
-#     if ind % 2 == 0:
-#       enc = '0' + enc
-#     else:
-#       enc = '1' + enc
-#     ind = ind/2
-#   return enc
 
 # Encode data using tree
 def get_encodings(node, enc, encodings):
@@ -84,50 +44,55 @@ def print_encodings(encodings):
   for k, v in encodings.iteritems():
     print k,":",v
 
-
+# Pad bitstrings to length 32
 def pad_int_string_to_32(s):
   return '0'*(32 - len(s)) + s
 
-
+# Main compress function
 def compress(input_file, output_file=None):
-  # Calculate char frequencies
   if not output_file: output_file = "compressed_"+input_file
-
+  # Read in orig data
   with open(input_file, 'r') as f:
     data = f.read()
   
-  # print data[:1024]
   char_freqs = get_char_freqs(data)
 
+  # Build list of [char, freq] values sorted by frequency
   freqs = map(list, sorted(char_freqs.iteritems(), key=lambda (k,v):(v,k))[::-1])
-  num_leaves = len(freqs)
 
+  # Build Huffman tree in nested list format
   nested_list_tree = build_tree_like_a_sane_person(freqs)
 
+  # Build dict of char : encoding strings
   encodings = {}
   get_encodings(nested_list_tree[0], '', encodings)  
 
-  # Build encoded string of data from encodings
+  # Build bitstring of data from encodings
   encoded = ''
   for char in data:
     encoded += encodings[char]
 
-  # Pack encoded data using struct
+  # Convert bitstrings into corresponding int values
   to_pack = [int(encoded[i:i+32], 2) for i in range(0, len(encoded)+1, 32)]
+  # Store length of bitstring of last data chunk -> could be smaller than 32 bits, and padded with extra zeros
   last_int_string_length = len(bin(to_pack[-1])[2:])
+  # Pack encoded data using struct
   packed_data = struct.pack("=%sI" % len(to_pack), *to_pack)
  
   # Write packed data to file
   with open(output_file, 'w') as f:
+    # Size of repr of Huffman tree to read the tree
     f.write(str(len(str(nested_list_tree)))+'\n')
     f.write(str(nested_list_tree))
+    # Number of chunks/ints packed
     f.write(str(len(to_pack))+'\n')
+    # Size of last chunk packed
     f.write(str(last_int_string_length)+'\n')
     f.write(packed_data)
 
 
 def decode_data(tree, unpacked_string):
-  # Decode extracted unpacked data
+  # Decode extracted unpacked data using stored Huffman tree
   decoded = ''
   counter = 0
   while counter < len(unpacked_string)-1:
@@ -149,25 +114,23 @@ def decompress(input_file, output_file=None):
   if not output_file: output_file = 'decompressed_'+input_file
 
   with open(input_file, 'r') as f:
-    # Read compressed data
+    # Read compressed data in order of packing
     tree_length = int(f.readline().rstrip())
     tree = eval(f.read(tree_length))
-    # print tree
     data_length = int(f.readline().rstrip())
     last_int_string_length = int(f.readline().rstrip())
     packed_data = f.read()
 
+  # Unpack ints from compressed data
   unpacked_ints = struct.unpack("=%sI" % data_length, packed_data)
-  # print unpacked_ints
-
+  # Create list of corresponding binary strings from ints
   unpacked_strings = [pad_int_string_to_32(bin(i)[2:]) for i in unpacked_ints]
-  # print unpacked_strings
-
+  # Concat entire data into one bitstring
   unpacked_string = ''.join(unpacked_strings)
+  # Get rid of padded zeros from last data chunk, if any
   unpacked_string = unpacked_string[:-32] + unpacked_string[-last_int_string_length:]
 
   decoded = decode_data(tree, unpacked_string)
-  # print decoded[:1024]
 
   # Write decompressed data to new file
   with open(output_file, 'w') as f:
